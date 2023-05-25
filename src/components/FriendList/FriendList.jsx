5ca853a620e1fac80364630cf14790ef6d958aac
VernihoraIvan <vernihorai@gmail.com>
2023-05-25 13:06:37 +0200

    created FriendsList component

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+// import PropTypes from 'prop-types';
+import css from './FriendList.module.css'
+import FriendListItem from '../FriendListItem/FriendListItem'
+ 
+function FriendList({friends}) {
+    return (
+        <ul className={css.friend_list}>
+
+{friends.map(({avatar, name, isOnline, id}) => (
+        <FriendListItem 
+        key={id}
+        name={name}
+        avatar={avatar}
+        isOnline={isOnline}
+        />
+     ))}
+         </ul>
+    )
+}
+
+export default FriendList
